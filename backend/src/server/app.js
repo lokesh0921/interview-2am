e46cb44a4f19f48c697562f29faf21b4f3530f3e
Lokesh Pawar <lokeshpawar921@gmail.com>
2025-09-20 07:45:09 +0530

    try to solve cors error by removing spedcial link

--- backend/src/server/app.js
+++ backend/src/server/app.js
@@ -13,7 +13,7 @@ const config = loadConfig();
 
 const app = express();
 app.disable("x-powered-by");
-app.use(cors({ origin: config.CORS_ORIGIN.split(","), credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
